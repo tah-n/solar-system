3ab4909acdf57b6255b3938f743a39719b8a50a4
tah-n <taherenjfi8@gmail.com>
2025-03-15 21:29:53 +0330

    first commit

--- src/main.js
+++ src/main.js
@@ -11,20 +11,20 @@ scene.fog = false;
 //textures 
 const textureLoader = new THREE.TextureLoader();
 const cubeTextureLoader = new THREE.CubeTextureLoader();
-cubeTextureLoader.setPath('static/textures/cubeMap/');
+cubeTextureLoader.setPath('/textures/cubeMap/');
 
 
-const sunTexture = textureLoader.load('static/textures/2k_sun.jpg');
-const earthTexture = textureLoader.load('static/textures/2k_earth_daymap.jpg');
-const moonTexture = textureLoader.load('static/textures/2k_moon.jpg');
-const marsTexture = textureLoader.load('static/textures/2k_mars.jpg');
-const venusTexture = textureLoader.load('static/textures/2k_venus_surface.jpg');
-const mercuryTexture = textureLoader.load('static/textures/2k_mercury.jpg');
-const jupiterTexture = textureLoader.load('static/textures/jupitermap.jpg');
-const saturnTexture = textureLoader.load('static/textures/2k_saturn.jpg');
-const uranusTexture = textureLoader.load('static/textures/2k_uranus.jpg');
-const neptuneTexture = textureLoader.load('static/textures/2k_neptune.jpg');
-const ringtexture = textureLoader.load('static/textures/2k_saturn_ring_alpha.png')
+const sunTexture = textureLoader.load('textures/2k_sun.jpg');
+const earthTexture = textureLoader.load('textures/2k_earth_daymap.jpg');
+const moonTexture = textureLoader.load('textures/2k_moon.jpg');
+const marsTexture = textureLoader.load('textures/2k_mars.jpg');
+const venusTexture = textureLoader.load('/textures/2k_venus_surface.jpg');
+const mercuryTexture = textureLoader.load('/textures/2k_mercury.jpg');
+const jupiterTexture = textureLoader.load('/textures/jupitermap.jpg');
+const saturnTexture = textureLoader.load('/textures/2k_saturn.jpg');
+const uranusTexture = textureLoader.load('/textures/2k_uranus.jpg');
+const neptuneTexture = textureLoader.load('/textures/2k_neptune.jpg');
+const ringtexture = textureLoader.load('/textures/2k_saturn_ring_alpha.png')
 
 const backgroundCubeMap = cubeTextureLoader.load([
         'px.png',
